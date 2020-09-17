2a4eedd7c9dcdf38773ecae72206947c54c3c161
PabloMombiela <pablomombiela@live.com.ar>
2020-09-17 16:32:39 -0300

    1er modificacion, se da acceso a las img

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express()
 const path = require('path')
 
 
-
 app.listen(3030, () => {
 
     console.log('server ok')
 })
+
+
+app.use (express.static ( __dirname +'/public')) 
+
 
 app.get('/', function (req, res) {
 
@@ -19,7 +22,7 @@ app.get('/', function (req, res) {
 
 app.get('/index.css', function (req, res) {
 
-    let file = path.resolve('public/index.css')
+    let file = path.resolve('style/index.css')
     res.sendFile(file)
 
 })
